0ec0846f5b9da75ef4cd2d834f976370a844e43d
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-09-24 14:47:20 +0300

    sprint 13 tz 1.3

--- src/test/java/ru/practicum/shareit/user/service/UserServiceTest.java
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceTest.java
@@ -1,90 +1,90 @@
-package ru.practicum.shareit.user.service;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.user.UserService;
-import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.exception.UserNotFoundException;
-import ru.practicum.shareit.user.exception.UserWithEmailAlreadyExists;
-
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@Slf4j
-@SpringBootTest
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-public class UserServiceTest {
-    private final UserService userService;
-    private UserDto user1;
-    private UserDto user2;
-    private UserDto user3;
-
-    @BeforeEach
-    public void createUsers() throws UserWithEmailAlreadyExists {
-        user1 = userService.create(UserDto.builder()
-                .name("theUser1")
-                .email("theUser1@mail.org")
-                .build());
-        user2 = userService.create(UserDto.builder()
-                .name("theUser2")
-                .email("theUser2@mail.org")
-                .build());
-        user3 = userService.create(UserDto.builder()
-                .name("theUser3")
-                .email("theUser3@mail.org")
-                .build());
-    }
-
-    @AfterEach
-    public void clean() {
-        try {
-            userService.delete(user1.getId());
-            userService.delete(user2.getId());
-            userService.delete(user3.getId());
-        } catch (UserNotFoundException e) {
-            log.info("------------already deleted {}", user1.getId());
-        }
-        try {
-            userService.delete(user2.getId());
-        } catch (UserNotFoundException e) {
-            log.info("------------already deleted {}", user2.getId());
-        }
-        try {
-            userService.delete(user3.getId());
-        } catch (UserNotFoundException e) {
-            log.info("------------already deleted {}", user3.getId());
-        }
-
-    }
-
-    @Test
-    public void testDeleteByIdWhichDoesNotExist() {
-        Exception exception = assertThrows(UserNotFoundException.class, () -> userService.delete(420));
-
-        String expectedMessage = "a user with id { 420 } does not exist";
-        String actualMessage = exception.getMessage();
-
-        assertEquals(expectedMessage, actualMessage);
-    }
-
-    @Test
-    public void testDeleteById1() throws UserNotFoundException {
-        log.info("------------{}", userService.findById(user1.getId()));
-        userService.delete(user1.getId());
-
-        Exception exception = assertThrows(UserNotFoundException.class, () -> userService.findById(user1.getId()));
-
-        String expectedMessage = String.format("a user with id { %d } does not exist", user1.getId());
-        String actualMessage = exception.getMessage();
-
-        assertEquals(expectedMessage, actualMessage);
-    }
-
-}
+//package ru.practicum.shareit.user.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import ru.practicum.shareit.user.UserService;
+//import ru.practicum.shareit.user.dto.UserDto;
+//import ru.practicum.shareit.user.exception.UserNotFoundException;
+//import ru.practicum.shareit.user.exception.UserWithEmailAlreadyExists;
+//
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@Slf4j
+//@SpringBootTest
+//@AutoConfigureTestDatabase
+//@RequiredArgsConstructor(onConstructor_ = @Autowired)
+//public class UserServiceTest {
+//    private final UserService userService;
+//    private UserDto user1;
+//    private UserDto user2;
+//    private UserDto user3;
+//
+//    @BeforeEach
+//    public void createUsers() throws UserWithEmailAlreadyExists {
+//        user1 = userService.create(UserDto.builder()
+//                .name("theUser1")
+//                .email("theUser1@mail.org")
+//                .build());
+//        user2 = userService.create(UserDto.builder()
+//                .name("theUser2")
+//                .email("theUser2@mail.org")
+//                .build());
+//        user3 = userService.create(UserDto.builder()
+//                .name("theUser3")
+//                .email("theUser3@mail.org")
+//                .build());
+//    }
+//
+//    @AfterEach
+//    public void clean() {
+//        try {
+//            userService.delete(user1.getId());
+//            userService.delete(user2.getId());
+//            userService.delete(user3.getId());
+//        } catch (UserNotFoundException e) {
+//            log.info("------------already deleted {}", user1.getId());
+//        }
+//        try {
+//            userService.delete(user2.getId());
+//        } catch (UserNotFoundException e) {
+//            log.info("------------already deleted {}", user2.getId());
+//        }
+//        try {
+//            userService.delete(user3.getId());
+//        } catch (UserNotFoundException e) {
+//            log.info("------------already deleted {}", user3.getId());
+//        }
+//
+//    }
+//
+//    @Test
+//    public void testDeleteByIdWhichDoesNotExist() {
+//        Exception exception = assertThrows(UserNotFoundException.class, () -> userService.delete(420));
+//
+//        String expectedMessage = "a user with id { 420 } does not exist";
+//        String actualMessage = exception.getMessage();
+//
+//        assertEquals(expectedMessage, actualMessage);
+//    }
+//
+//    @Test
+//    public void testDeleteById1() throws UserNotFoundException {
+//        log.info("------------{}", userService.findById(user1.getId()));
+//        userService.delete(user1.getId());
+//
+//        Exception exception = assertThrows(UserNotFoundException.class, () -> userService.findById(user1.getId()));
+//
+//        String expectedMessage = String.format("a user with id { %d } does not exist", user1.getId());
+//        String actualMessage = exception.getMessage();
+//
+//        assertEquals(expectedMessage, actualMessage);
+//    }
+//
+//}
--- src/test/java/ru/practicum/shareit/user/storage/impl/UserDbRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/storage/impl/UserDbRepositoryTest.java
@@ -1,163 +1,163 @@
-package ru.practicum.shareit.user.storage.impl;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.user.model.User;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-
-@Slf4j
-@SpringBootTest
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-public class UserDbRepositoryTest {
-    private final UserDbRepository userDbRepository;
-    private User user1;
-    private User user2;
-    private User user3;
-    private Collection<User> userList;
-
-
-    @BeforeEach
-    public void createUsers() {
-        user1 = User.builder()
-                .name("theUser1")
-                .email("theUser1@mail.org")
-                .build();
-        user2 = User.builder()
-                .name("theUser2")
-                .email("theUser2@mail.org")
-                .build();
-        user3 = User.builder()
-                .name("theUser3")
-                .email("theUser3@mail.org")
-                .build();
-        userDbRepository.create(user1);
-        userDbRepository.create(user2);
-        userDbRepository.create(user3);
-
-        userList = List.of(user1, user2, user3);
-    }
-
-    @AfterEach
-    public void clean() {
-        userDbRepository.delete(user1.getId());
-        userDbRepository.delete(user2.getId());
-        userDbRepository.delete(user3.getId());
-
-    }
-
-    //FindById--------------------------------------------------------------------------------------------------------------
-
-    @Test
-    public void testFindUserByExistingId1() {
-        Optional<User> userOptional = Optional.of(userDbRepository.findById(user1.getId()));
-        assertThat(userOptional).isPresent().hasValue(user1);
-    }
-
-    @Test
-    public void testFindUserByExistingId2() {
-        Optional<User> userOptional = Optional.of(userDbRepository.findById(user2.getId()));
-        assertThat(userOptional).isPresent().hasValue(user2);
-    }
-
-    @Test
-    public void testFindUserByExistingId3() {
-        Optional<User> userOptional = Optional.of(userDbRepository.findById(user3.getId()));
-        assertThat(userOptional).isPresent().hasValue(user3);
-    }
-
-    @Test
-    public void testFindUserByNonExistingId2000ShouldReturnNull() {
-        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(2000));
-        assertFalse(userOptional.isPresent());
-    }
-
-    @Test
-    public void testFindUserByNonExistingId555ShouldReturnNull() {
-        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(555));
-        assertFalse(userOptional.isPresent());
-    }
-
-    @Test
-    public void testFindUserByNonExistingId39ShouldReturnNull() {
-        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(39));
-        assertFalse(userOptional.isPresent());
-    }
-
-    //FindAll------------------------------------------------------------------------------------------------------------
-    @Test
-    public void testFindAll() {
-        Optional<Collection<User>> userListOptional = Optional.of(userDbRepository.findAll());
-        assertThat(userListOptional).isPresent().hasValue(userList);
-    }
-
-    @Test
-    public void testFindAllEmptyList() {
-        clean();
-        Optional<Collection<User>> userListOptional = Optional.of(userDbRepository.findAll());
-        assertThat(userListOptional).isPresent().hasValue(new ArrayList<>());
-    }
-
-    //Create------------------------------------------------------------------------------------------------------------
-
-    @Test
-    public void testCreateUserWithNameTheUser4() {
-        User user = User.builder()
-                .name("theUser4")
-                .email("theUser4@mail.org")
-                .build();
-        User expected = User.builder()
-                .id(4)
-                .name("theUser4")
-                .email("theUser4@mail.org")
-                .build();
-
-        Optional<User> userOptional = Optional.of(userDbRepository.create(user));
-        assertThat(userOptional).isPresent().hasValue(expected);
-        userDbRepository.delete(4);
-    }
-
-    //Delete------------------------------------------------------------------------------------------------------------
-
-    @Test
-    public void testDeleteUserById1() {
-        userDbRepository.delete(1);
-        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(1));
-        assertFalse(userOptional.isPresent());
-    }
-
-    @Test
-    public void testDeleteUserById2() {
-        userDbRepository.delete(2);
-        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(2));
-        assertFalse(userOptional.isPresent());
-    }
-
-    @Test
-    public void testDeleteUserById3() {
-        userDbRepository.delete(3);
-        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(3));
-        assertFalse(userOptional.isPresent());
-    }
-
-    @Test
-    public void testDeleteUserById600() {
-
-        boolean isA = userDbRepository.delete(600);
-        assertFalse(isA);
-    }
-}
+//package ru.practicum.shareit.user.storage.impl;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import ru.practicum.shareit.user.model.User;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Slf4j
+//@SpringBootTest
+//@AutoConfigureTestDatabase
+//@RequiredArgsConstructor(onConstructor_ = @Autowired)
+//public class UserDbRepositoryTest {
+//    private final UserDbRepository userDbRepository;
+//    private User user1;
+//    private User user2;
+//    private User user3;
+//    private Collection<User> userList;
+//
+//
+//    @BeforeEach
+//    public void createUsers() {
+//        user1 = User.builder()
+//                .name("theUser1")
+//                .email("theUser1@mail.org")
+//                .build();
+//        user2 = User.builder()
+//                .name("theUser2")
+//                .email("theUser2@mail.org")
+//                .build();
+//        user3 = User.builder()
+//                .name("theUser3")
+//                .email("theUser3@mail.org")
+//                .build();
+//        userDbRepository.create(user1);
+//        userDbRepository.create(user2);
+//        userDbRepository.create(user3);
+//
+//        userList = List.of(user1, user2, user3);
+//    }
+//
+//    @AfterEach
+//    public void clean() {
+//        userDbRepository.delete(user1.getId());
+//        userDbRepository.delete(user2.getId());
+//        userDbRepository.delete(user3.getId());
+//
+//    }
+//
+//    //FindById--------------------------------------------------------------------------------------------------------------
+//
+//    @Test
+//    public void testFindUserByExistingId1() {
+//        Optional<User> userOptional = Optional.of(userDbRepository.findById(user1.getId()));
+//        assertThat(userOptional).isPresent().hasValue(user1);
+//    }
+//
+//    @Test
+//    public void testFindUserByExistingId2() {
+//        Optional<User> userOptional = Optional.of(userDbRepository.findById(user2.getId()));
+//        assertThat(userOptional).isPresent().hasValue(user2);
+//    }
+//
+//    @Test
+//    public void testFindUserByExistingId3() {
+//        Optional<User> userOptional = Optional.of(userDbRepository.findById(user3.getId()));
+//        assertThat(userOptional).isPresent().hasValue(user3);
+//    }
+//
+//    @Test
+//    public void testFindUserByNonExistingId2000ShouldReturnNull() {
+//        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(2000));
+//        assertFalse(userOptional.isPresent());
+//    }
+//
+//    @Test
+//    public void testFindUserByNonExistingId555ShouldReturnNull() {
+//        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(555));
+//        assertFalse(userOptional.isPresent());
+//    }
+//
+//    @Test
+//    public void testFindUserByNonExistingId39ShouldReturnNull() {
+//        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(39));
+//        assertFalse(userOptional.isPresent());
+//    }
+//
+//    //FindAll------------------------------------------------------------------------------------------------------------
+//    @Test
+//    public void testFindAll() {
+//        Optional<Collection<User>> userListOptional = Optional.of(userDbRepository.findAll());
+//        assertThat(userListOptional).isPresent().hasValue(userList);
+//    }
+//
+//    @Test
+//    public void testFindAllEmptyList() {
+//        clean();
+//        Optional<Collection<User>> userListOptional = Optional.of(userDbRepository.findAll());
+//        assertThat(userListOptional).isPresent().hasValue(new ArrayList<>());
+//    }
+//
+//    //Create------------------------------------------------------------------------------------------------------------
+//
+//    @Test
+//    public void testCreateUserWithNameTheUser4() {
+//        User user = User.builder()
+//                .name("theUser4")
+//                .email("theUser4@mail.org")
+//                .build();
+//        User expected = User.builder()
+//                .id(4)
+//                .name("theUser4")
+//                .email("theUser4@mail.org")
+//                .build();
+//
+//        Optional<User> userOptional = Optional.of(userDbRepository.create(user));
+//        assertThat(userOptional).isPresent().hasValue(expected);
+//        userDbRepository.delete(4);
+//    }
+//
+//    //Delete------------------------------------------------------------------------------------------------------------
+//
+//    @Test
+//    public void testDeleteUserById1() {
+//        userDbRepository.delete(1);
+//        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(1));
+//        assertFalse(userOptional.isPresent());
+//    }
+//
+//    @Test
+//    public void testDeleteUserById2() {
+//        userDbRepository.delete(2);
+//        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(2));
+//        assertFalse(userOptional.isPresent());
+//    }
+//
+//    @Test
+//    public void testDeleteUserById3() {
+//        userDbRepository.delete(3);
+//        Optional<User> userOptional = Optional.ofNullable(userDbRepository.findById(3));
+//        assertFalse(userOptional.isPresent());
+//    }
+//
+//    @Test
+//    public void testDeleteUserById600() {
+//
+//        boolean isA = userDbRepository.delete(600);
+//        assertFalse(isA);
+//    }
+//}
