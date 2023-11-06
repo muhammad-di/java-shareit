d5d20e19b23d9a08a3ab2cb7d943a7b5f7994765
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-11-06 10:01:30 +0300

    sprint 15 tz 2.8

--- src/test/java/ru/practicum/shareit/booking/dto/BookingDtoTest.java
+++ src/test/java/ru/practicum/shareit/booking/dto/BookingDtoTest.java
@@ -2,9 +2,12 @@ package ru.practicum.shareit.booking.dto;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.booking.mapper.BookingMapping;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
@@ -15,25 +18,24 @@ import static org.hamcrest.Matchers.equalTo;
 public class BookingDtoTest {
 
 
-    private User userCopy1;
-    private User booker2;
-    private Booking booking1;
-    private Booking bookingCopy1;
+    private UserDto bookerDto2;
+    private BookingDto bookingDto1;
+    private BookingDto bookingCopyDto1;
     LocalDateTime testStart;
     LocalDateTime testEnd;
 
 
     @BeforeEach
     public void create() {
-         testStart = LocalDateTime.of(2023, 11, 2, 3, 15);
-         testEnd = LocalDateTime.of(2023, 11, 2, 5, 15);
+        testStart = LocalDateTime.of(2023, 11, 2, 3, 15);
+        testEnd = LocalDateTime.of(2023, 11, 2, 5, 15);
 
         User user1 = User.builder().id(1).name("John").email("john.doe@mail.com").build();
-        userCopy1 = User.builder().id(1).name("John").email("john.doe@mail.com").build();
         User booker1 = User.builder().id(2).name("John2").email("john2.doe@mail.com").build();
-        booker2 = User.builder().id(3).name("John2").email("john2.doe@mail.com").build();
+        User booker2 = User.builder().id(3).name("John2").email("john2.doe@mail.com").build();
+        bookerDto2 = UserMapper.toUserDto(booker2);
         Item item1 = Item.builder().id(1).name("item1").description("description1").available(true).owner(user1).build();
-        booking1 = Booking.builder()
+        Booking booking1 = Booking.builder()
                 .id(1)
                 .start(testStart)
                 .end(testEnd)
@@ -41,7 +43,7 @@ public class BookingDtoTest {
                 .item(item1)
                 .status(Status.WAITING)
                 .build();
-        bookingCopy1 = Booking.builder()
+        Booking bookingCopy1 = Booking.builder()
                 .id(1)
                 .start(testStart)
                 .end(testEnd)
@@ -49,31 +51,34 @@ public class BookingDtoTest {
                 .item(item1)
                 .status(Status.WAITING)
                 .build();
+
+        bookingDto1 = BookingMapping.toBookingDto(booking1);
+        bookingCopyDto1 = BookingMapping.toBookingDto(bookingCopy1);
     }
 
     @Test
     void testEqual() throws Exception {
-        assertThat(booking1, equalTo(bookingCopy1));
+        assertThat(bookingDto1, equalTo(bookingCopyDto1));
     }
 
     @Test
     void testHash() throws Exception {
-        assertThat(booking1.hashCode(), equalTo(bookingCopy1.hashCode()));
+        assertThat(bookingDto1.hashCode(), equalTo(bookingCopyDto1.hashCode()));
     }
 
     @Test
     void testToString() throws Exception {
-        assertThat(booking1.toString(), equalTo(bookingCopy1.toString()));
+        assertThat(bookingDto1.toString(), equalTo(bookingCopyDto1.toString()));
     }
 
     @Test
     void testGetters() throws Exception {
-        assertThat(booking1.getId(), equalTo(bookingCopy1.getId()));
-        assertThat(booking1.getEnd(), equalTo(bookingCopy1.getEnd()));
-        assertThat(booking1.getStart(), equalTo(bookingCopy1.getStart()));
-        assertThat(booking1.getStatus(), equalTo(bookingCopy1.getStatus()));
-        assertThat(booking1.getBooker(), equalTo(bookingCopy1.getBooker()));
-
+        assertThat(bookingDto1.getId(), equalTo(bookingCopyDto1.getId()));
+        assertThat(bookingDto1.getEnd(), equalTo(bookingCopyDto1.getEnd()));
+        assertThat(bookingDto1.getStart(), equalTo(bookingCopyDto1.getStart()));
+        assertThat(bookingDto1.getStatus(), equalTo(bookingCopyDto1.getStatus()));
+        assertThat(bookingDto1.getBooker(), equalTo(bookingCopyDto1.getBooker()));
+        assertThat(bookingDto1.getItem(), equalTo(bookingCopyDto1.getItem()));
     }
 
     @Test
@@ -83,22 +88,23 @@ public class BookingDtoTest {
         s.plusHours(2);
         e.plusHours(2);
 
-        booking1.setId(2);
-        bookingCopy1.setId(2);
-        booking1.setEnd(e);
-        bookingCopy1.setEnd(e);
-        booking1.setStart(s);
-        bookingCopy1.setStart(s);
-        booking1.setStatus(Status.APPROVED);
-        bookingCopy1.setStatus(Status.APPROVED);
-        booking1.setBooker(booker2);
-        bookingCopy1.setBooker(booker2);
+        bookingDto1.setId(2);
+        bookingCopyDto1.setId(2);
+        bookingDto1.setEnd(e);
+        bookingCopyDto1.setEnd(e);
+        bookingDto1.setStart(s);
+        bookingCopyDto1.setStart(s);
+        bookingDto1.setStatus(Status.APPROVED);
+        bookingCopyDto1.setStatus(Status.APPROVED);
+        bookingDto1.setBooker(bookerDto2);
+        bookingCopyDto1.setBooker(bookerDto2);
 
-        assertThat(booking1.getId(), equalTo(bookingCopy1.getId()));
-        assertThat(booking1.getEnd(), equalTo(bookingCopy1.getEnd()));
-        assertThat(booking1.getStart(), equalTo(bookingCopy1.getStart()));
-        assertThat(booking1.getStatus(), equalTo(bookingCopy1.getStatus()));
-        assertThat(booking1.getBooker(), equalTo(bookingCopy1.getBooker()));
+        assertThat(bookingDto1.getId(), equalTo(bookingCopyDto1.getId()));
+        assertThat(bookingDto1.getEnd(), equalTo(bookingCopyDto1.getEnd()));
+        assertThat(bookingDto1.getStart(), equalTo(bookingCopyDto1.getStart()));
+        assertThat(bookingDto1.getStatus(), equalTo(bookingCopyDto1.getStatus()));
+        assertThat(bookingDto1.getBooker(), equalTo(bookingCopyDto1.getBooker()));
+        assertThat(bookingDto1.getItem(), equalTo(bookingCopyDto1.getItem()));
     }
 
 
