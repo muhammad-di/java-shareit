f5f8f7e97f9e62cdd54b8762df6e7f0f81d81e80
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-11-06 10:22:34 +0300

    sprint 15 tz 2.9

---
+++ src/test/java/ru/practicum/shareit/booking/exception/InvalidEndTimeExceptionTest.java
@@ -0,0 +1,40 @@
+package ru.practicum.shareit.booking.exception;
+
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+        import org.springframework.http.HttpStatus;
+
+        import static org.hamcrest.MatcherAssert.assertThat;
+        import static org.hamcrest.Matchers.equalTo;
+
+public class InvalidEndTimeExceptionTest {
+
+
+    private InvalidEndTimeException exception;
+
+
+    @BeforeEach
+    public void create() {
+        exception = new InvalidEndTimeException("test message");
+        exception.setErrorCode(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void testGetErrorCode() {
+        assertThat(exception.getErrorCode(), equalTo(HttpStatus.NOT_FOUND));
+    }
+
+    @Test
+    void testSetErrorCode() {
+        exception.setErrorCode(HttpStatus.BAD_REQUEST);
+        assertThat(exception.getErrorCode(), equalTo(HttpStatus.BAD_REQUEST));
+    }
+
+    @Test
+    void testGetErrorMessage() {
+        assertThat(exception.getErrorMessage(), equalTo("MASSAGE: test message; ERROR CODE: 404 NOT_FOUND"));
+    }
+
+
+}
+
